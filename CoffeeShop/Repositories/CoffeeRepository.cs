5ba501c7e46f4bb32926e7d69f3ad47b6e9bad50
Ben Watts <brandolphwatts@gmail.com>
2022-07-05 13:09:19 -0500

    initial commit

---
+++ CoffeeShop/Repositories/CoffeeRepository.cs
@@ -0,0 +1,166 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.Data.SqlClient;
+using Microsoft.Extensions.Configuration;
+using CoffeeShop.Models;
+
+namespace CoffeeShop.Repositories
+{
+    public class CoffeeRepository : ICoffeeRepository
+    {
+        private readonly string _connectionString;
+        public CoffeeRepository(IConfiguration configuration)
+        {
+            _connectionString = configuration.GetConnectionString("DefaultConnection");
+        }
+
+        private SqlConnection Connection
+        {
+            get { return new SqlConnection(_connectionString); }
+        }
+
+        public List<Coffee> GetAll()
+        {
+            using (var conn = Connection)
+            {
+                conn.Open();
+                using (var cmd = conn.CreateCommand())
+                {
+                    cmd.CommandText = @"SELECT c.Id, c.Title, bv.Name, c.BeanVarietyId,  bv.Region, bv.Notes From Coffee c
+                                        LEFT JOIN BeanVariety bv ON c.BeanVarietyId = bv.Id";
+                    using (SqlDataReader reader = cmd.ExecuteReader())
+                    {
+                        var coffees = new List<Coffee>();
+                        while (reader.Read())
+                        {
+                            var coffee = new Coffee()
+                            {
+                                Id = reader.GetInt32(reader.GetOrdinal("Id")),
+                                Title = reader.GetString(reader.GetOrdinal("Title")),
+                                BeanVarietyId = reader.GetInt32(reader.GetOrdinal("BeanVarietyId")),
+                            };
+                            BeanVariety beanVariety = new BeanVariety()
+                            {
+                                Name = reader.GetString(reader.GetOrdinal("Name")),
+                                Region = reader.GetString(reader.GetOrdinal("Region")),
+                                
+                            };
+
+                            if (!reader.IsDBNull(reader.GetOrdinal("Notes")))
+                            {
+                                beanVariety.Notes = reader.GetString(reader.GetOrdinal("Notes"));
+                            }
+
+                            coffee.BeanVariety = beanVariety;
+                            coffees.Add(coffee);
+                        }
+
+                        return coffees;
+                    }
+                }
+            }
+        }
+
+        public Coffee Get(int id)
+        {
+            using (var conn = Connection)
+            {
+                conn.Open();
+                using (var cmd = conn.CreateCommand())
+                {
+                    cmd.CommandText = @"
+                        SELECT c.Id, c.Title, bv.Name, c.BeanVarietyId,  bv.Region, bv.Notes From Coffee c
+                                        LEFT JOIN BeanVariety bv ON c.BeanVarietyId = bv.Id
+                         WHERE c.Id = @id;";
+                    cmd.Parameters.AddWithValue("@id", id);
+
+                    using (SqlDataReader reader = cmd.ExecuteReader())
+                    {
+                        Coffee coffee = null;
+                        if (reader.Read())
+                        {
+                            coffee = new Coffee()
+                            {
+                                Id = reader.GetInt32(reader.GetOrdinal("Id")),
+                                Title = reader.GetString(reader.GetOrdinal("Title")),
+                                BeanVarietyId = reader.GetInt32(reader.GetOrdinal("BeanVarietyId")),
+                            };
+                            BeanVariety beanVariety = new BeanVariety()
+                            {
+                                Name = reader.GetString(reader.GetOrdinal("Name")),
+                                Region = reader.GetString(reader.GetOrdinal("Region")),
+
+                            };
+
+                            if (!reader.IsDBNull(reader.GetOrdinal("Notes")))
+                            {
+                                beanVariety.Notes = reader.GetString(reader.GetOrdinal("Notes"));
+                            }
+                            coffee.BeanVariety = beanVariety;
+                        }
+
+
+
+                        return coffee;
+                    }
+                }
+            }
+        }
+
+        public void Add(Coffee coffee)
+        {
+            using (var conn = Connection)
+            {
+                conn.Open();
+                using (var cmd = conn.CreateCommand())
+                {
+                    cmd.CommandText = @"
+                                    INSERT INTO Coffee (Title, BeanVarietyId)
+                                    OUTPUT INSERTED.ID
+                                    VALUES (@tiles, @beanVarieyId)";
+                    cmd.Parameters.AddWithValue("@title", coffee.Title);
+                    cmd.Parameters.AddWithValue("@beanVarietyId", coffee.BeanVarietyId);
+                    
+
+                    coffee.Id = (int)cmd.ExecuteScalar();
+                }
+            }
+        }
+
+        public void Update(Coffee coffee)
+        {
+            using (var conn = Connection)
+            {
+                conn.Open();
+                using (var cmd = conn.CreateCommand())
+                {
+                    cmd.CommandText = @"
+                                UPDATE BeanVariety 
+                                   SET Title = @title, 
+                                       BeanVarietyId = @beanVarietyId
+                                 WHERE Id = @id";
+                    cmd.Parameters.AddWithValue("@id", coffee.Id);
+                    cmd.Parameters.AddWithValue("@title", coffee.Title);
+                    cmd.Parameters.AddWithValue("@beanVarietyId", coffee.BeanVariety);
+                    
+                    cmd.ExecuteNonQuery();
+                }
+            }
+        }
+
+        public void Delete(int id)
+        {
+            using (var conn = Connection)
+            {
+                conn.Open();
+                using (var cmd = conn.CreateCommand())
+                {
+                    cmd.CommandText = "DELETE FROM Coffee WHERE Id = @id";
+                    cmd.Parameters.AddWithValue("@id", id);
+
+                    cmd.ExecuteNonQuery();
+                }
+            }
+        }
+    }
+}
